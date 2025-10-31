53f31df679f111b8eb22124a43816757647071a0
hamdane1548 <oussamahamdane80@gmail.com>
2025-10-31 21:03:35 +0100

    fix second problem

--- src/test.sh
+++ src/test.sh
@@ -1,6 +1,10 @@
 #!bin/bash
 EXPECTED="Hello, Alice!"
 OUTPUT=$(node -e "console.log(require('./src/App.js').default('Alice'))")
+echo $OUTPUT
+echo $EXPECTED
+SORTIE=$OUTPUT == $EXPECTED
+echo $SORTIE
 if [ "$OUTPUT" == "$EXPECTED" ]; then
     echo "Test passed!"
     exit 0
